f4e8f1e26a9c16e3920b3afe3693bb506a1c66dc
MDBBee <bobbyugbebor@gmail.com>
2024-11-24 10:40:37 +0200

    Pagination ✅

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -6,15 +6,8 @@ import FormCheckbox from './FormCheckBox';
 
 function Filters() {
   const { meta, products, urlParams } = useLoaderData();
-
   const { categories, companies, pagination } = meta;
-  // console.log(meta);
-  const maxV = Math.max(...products.map((p) => +p.attributes.price));
-  const minV = Math.min(...products.map((p) => +p.attributes.price));
-
   const { category, company, order, price, search, shipping } = urlParams;
-  // const shipping = urlParams.shipping || null;
-  console.log(urlParams);
 
   return (
     <Form className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
